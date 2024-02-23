0c298adaebb35db2bde1eaf181a8ffbc92c695a6
jay6943 <uetri@naver.com>
2024-02-23 17:17:16 +0900

    Dell workstation

---
+++ Standa_8SMC4_USB.py
@@ -0,0 +1,61 @@
+import time
+import libximc.highlevel as ximc
+
+dev_linear = r'xi-com:\\.\COM13'  # SN 32798 Axis 1
+dev_vertical = r'xi-com:\\.\COM14'  # SN 32835 Axis 2
+# dev_x1 = r'xi-com:\\.\COM9'  # SN 32800 Axis 1
+# dev_y1 = r'xi-com:\\.\COM10'  # SN 32776 Axis 2
+# dev_z1 = r'xi-com:\\.\COM5'  # SN
+# dev_z2 = r'xi-com:\\.\COM15'  # SN
+
+
+def get_info(device):
+  axis = ximc.Axis(device)
+  axis.open_device()
+  sn = str(axis.get_serial_number())
+  df = str(axis.get_engine_settings())
+  print(df)
+  axis.close_device()
+
+  fp = open('../data/Standa_' + sn + '.txt', 'w')
+  fp.write(str(device) + '\n' + str(df))
+  fp.close()
+
+
+def Standa_time():
+  axis = ximc.Axis(dev_linear)
+  axis.open_device()
+  axis.command_right()
+  time.sleep(3)
+  axis.command_stop()
+  axis.close_device()
+
+
+def Standa_8MT200_100():
+  axis = ximc.Axis(dev_linear)
+  axis.open_device()
+  axis.command_movr(20, 0)
+  axis.command_wait_for_stop(500)
+  axis.command_movr(-20, 0)
+  axis.command_wait_for_stop(500)
+  axis.command_stop()
+  axis.close_device()
+
+
+def Standa_8MVT70_13_1():
+  axis = ximc.Axis(dev_vertical)
+  axis.open_device()
+  # axis.command_home()
+  # axis.command_wait_for_stop(500)
+  axis.command_right()
+  time.sleep(3)
+  axis.command_stop()
+  axis.close_device()
+
+
+if __name__ == '__main__':
+  # Standa_8MT200_100()
+
+  get_info(dev_linear)
+  get_info(dev_vertical)
+  # Standa_8MVT70_13_1()
