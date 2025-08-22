6717ba18a9c2c57a373338325d3cb6546b858eb5
jay6943 <uetri@naver.com>
2025-08-22 09:07:25 +0900

    Dell Workstation

--- Maiman_Bench.py
+++ Maiman_Bench.py
@@ -1,57 +1,61 @@
-import dev
-import numpy as np
-import matplotlib.pyplot as plt
+import time
+import serial
 
 
-def test():
-    mbl = dev.Maiman_Laser_TEC()
-    # mbl.write('curr:value 1')
-    # mbl.write('dev:start on')
-    # mbl.write('dur:value 1')
-    # mbl.write('freq:value 0')
-    # mbl.write('tec:temp:value 25')
-    mbl.write('dev:blo:ign')
-    # print(mbl.read('block?'))
-    # print(mbl.read('tec:temp:real?'))
-    mbl.close()
+class Maiman_Laser_TEC:
+  def __init__(self):
+    self.device = serial.Serial('COM4', 115200)
+
+  def write(self, command):
+    self.device.write(bytearray(f'{command}\r', 'utf-8'))
+    time.sleep(0.5)
+
+  def query(self, command):
+    self.write(command)
+    data = self.device.read(self.device.in_waiting)
+    return data.decode().split(' ')[-1]
+
+  def read(self, command):
+    return float(self.query(command))
+
+  def close(self):
+    self.device.close()
+
+  def set_current(self, current):
+    self.write(f'curr:value {current:.4f}')
+    return self.read('volt:real?')
+
+  def set_temperature(self, temperature):
+    self.write(f'tec:temp:value {temperature:.1f}')
 
 
-def laser(filname):
-  x = np.linspace(1, 100, 100)
-  y = np.zeros_like(x)
-  v = np.zeros_like(x)
-
-  iv = dev.Maiman_Laser_TEC()
-  iv.write('*RST')
-  iv.write(f'curr:value {np.round(x[0], 3)}')
-  iv.write('dev:start on')
-
-  pd = dev.Keysight_81630B_photodiode()
-  pd.mW(1, 1)
-  for i in range(len(x)):
-    iv.write(f'curr:value {np.round(x[i], 3)}')
-    v[i] = float(iv.read('volt:real?'))
-    y[i] = pd.fetch(1, 1) * 1e6
-    x[i] = float(iv.read('curr:real?'))
-    print(f'{x[i]:.3f} mA, {y[i]:.6f} uW, {v[i]:.3f} V')
-  pd.dBm(1, 1)
-  pd.close()
-
-  iv.write('dev:start off')
-  iv.close()
-
-  data = np.array([x, y, v]).transpose()
-  np.savetxt(f'{filname}.dat', data)
-
-  plt.figure(figsize=(10, 6))
-  plt.plot(x, y)
-  plt.xlabel('Current (mA)')
-  plt.ylabel(r'Output power ($\mu$W)')
-  plt.grid()
-  plt.savefig(f'{filname}.png')
-  plt.show()
+def reset():
+  ld = Maiman_Laser_TEC()
+  ld.write('*RST')
+  ld.close()
 
 
-if __name__ == '__main__':
-  # laser('D:/data/SOA/3SOA010-3/LIV_real')
-  test()
+def laser_on(current):
+  ld = Maiman_Laser_TEC()
+  ld.set_current(current)
+  ld.write('dev:start on')
+  ld.close()
+
+
+def laser_off():
+  ld = Maiman_Laser_TEC()
+  ld.write('dev:start off')
+  ld.close()
+
+
+def tec_on(temperature):
+  ld = Maiman_Laser_TEC()
+  ld.set_temperature(temperature)
+  ld.write('tec:start on')
+  ld.close()
+
+
+def tec_off():
+  ld = Maiman_Laser_TEC()
+  ld.write('tec:start off')
+  ld.close()
